1a4dfeb6140ceb228ef10383dc54230110b64d79
Untung Budiman <untungbudiman.dev@gmail.com>
2025-03-02 13:00:52 +0700

    create responsive navbar for mobile

--- src/components/SelectLang.tsx
+++ src/components/SelectLang.tsx
@@ -5,22 +5,44 @@ import USFlags from '../assets/icons/us.png'
 
 export default function SelectLang() {
     const { t, i18n } = useTranslation();
-
     return (
         <Select onValueChange={(value) => i18n.changeLanguage(value)} defaultValue={i18n.language}>
             <SelectTrigger className="w-[90px] ring-0 outline-none">
                 <div className='flex flex-row-reverse items-center justify-center gap-1.5'>
                     <SelectValue placeholder={i18n.language === 'en' ? 'English' : 'Indonesian'} />
                     {i18n.language === 'en' ? (
-                        <img src={USFlags} alt='Us Flag' className='size-5'/>
-                    ): (
-                        <img src={IDFlags} alt='Indonesian Flag' className='size-5'/>
+                        <img src={USFlags} alt='Us Flag' className='size-5' />
+                    ) : (
+                        <img src={IDFlags} alt='Indonesian Flag' className='size-5' />
                     )}
                 </div>
             </SelectTrigger>
             <SelectContent className='w-fit'>
                 <SelectItem value="id">ID</SelectItem>
                 <SelectItem value="en">EN</SelectItem>
+            </SelectContent>
+        </Select>
+
+    )
+}
+
+export function SelectLangMobile() {
+    const { t, i18n } = useTranslation();
+    return (
+        <Select onValueChange={(value) => i18n.changeLanguage(value)} defaultValue={i18n.language}>
+            <SelectTrigger className="w-2/3 ring-0 outline-none h-16 text-2xl">
+                <div className='flex flex-row-reverse items-center justify-center gap-3'>
+                    <SelectValue placeholder={i18n.language === 'en' ? t('languages.eng') : t('languages.indo')} className='text-2xl' />
+                    {i18n.language === 'en' ? (
+                        <img src={USFlags} alt='Us Flag' className='size-7' />
+                    ) : (
+                        <img src={IDFlags} alt='Indonesian Flag' className='size-7' />
+                    )}
+                </div>
+            </SelectTrigger>
+            <SelectContent className='w-fit'>
+                <SelectItem value="id" className='text-2xl'>{t('languages.indo')}</SelectItem>
+                <SelectItem value="en" className='text-2xl'>{t('languages.eng')}</SelectItem>
             </SelectContent>
         </Select>
 
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,10 +1,16 @@
 import ButtonTheme from '@/components/ButtonTheme'
 import SelectLang from '@/components/SelectLang'
+import { Button } from '@/components/ui/button';
 import { useTranslation } from 'react-i18next'
 import { Link, NavLink } from 'react-router'
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import Sidebar from './Sidebar';
+import { useState } from 'react';
+
 
 export default function Navbar() {
   const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
   const menuItem = [
     { name: 'home', link: '/' },
     { name: 'about', link: '/about' },
@@ -15,19 +21,28 @@ export default function Navbar() {
   return (
     <div className='w-full py-2'>
       <div className='bg-background py-2 rounded-sm px-5'>
-        <div className='flex items-center justify-between'>
-          <Link to="/" className='text-lg font-black'>Untung Budiman</Link>
-          <div className='flex items-center'>
+        <div className='flex items-center justify-between @container'>
+          <Link to="/" className='text-lg font-black logo flex items-center justify-center'>Untung Budiman</Link>
+          <div className='flex items-center @sm:hidden @md:hidden @xs:hidden @xl:flex'>
             {menuItem.map((item, index) => {
               return (
-                <NavLink key={index} to={item.link} className={({ isActive }) => isActive ? "nav-link border-b-2 border-primary" : "nav-link"} >{t('menu.'+item.name)}</NavLink>
+                <NavLink key={index} to={item.link} className={({ isActive }) => isActive ? "nav-link border-b-2 border-third" : "nav-link"} >{t('menu.' + item.name)}</NavLink>
               )
             })}
           </div>
-          <div className='flex items-center justify-center gap-1'>
+          <div className='flex items-center justify-center gap-1 @sm:hidden @md:hidden @xs:hidden @xl:flex'>
             <SelectLang />
-            <ButtonTheme className='cursor-pointer'/>
+            <ButtonTheme className='cursor-pointer' />
           </div>
+
+          <div className='@xl:hidden @md:hidden @sm:block @xs:block'>
+            <Button className='' variant="outline" onClick={() => setOpen(true)}>
+              <HiOutlineMenuAlt2 className='size-4' />
+            </Button>
+          </div>
+
+          <Sidebar isOpen={open} onClose={() => setOpen(!open)} />
+
         </div>
       </div>
     </div>
